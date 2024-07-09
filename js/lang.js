dd07e7cbc4dd1cd19831a1dd670ba989095d27e3
Julio-MP <julio.possebon1@gmail.com>
2024-07-09 00:51:35 -0300

    EndOfTheDayCommit

---
+++ js/lang.js
@@ -0,0 +1,39 @@
+const translations = {
+    'pt-br': {
+        title: 'Olá! <br>Bem-vindo ao <strong class="Strong1">meu Portfólio!</strong>',
+        description: 'Aqui você pode encontrar alguns dos meus projetos concluídos',
+        link1: 'Jogue meus jogos!',
+        link2: 'Abra meu GitHub',
+        link3: 'Meu LinkedIn',
+        link4: 'Sobre mim',
+        link5: 'Entre em Contato',
+    },
+    'en': {
+        title: 'Hello!<br> Welcome to <strong class="Strong1">my Portfolio!</strong>',
+        description: 'Here you can find some of my finish projects and more:',
+        link1: 'Play my Games!',
+        link2: 'Open my GitHub',
+        link3: "My LinkedIn",
+        link4: "About Me",
+        link5: 'Contact me',
+    },
+    'it': {
+        title: 'Ciao!<br> Benvenuto nel <strong class="Strong1">mio Portfolio!</strong>',
+        description: 'Qui puoi trovare alcuni dei miei progetti finiti',
+        link1: 'Gioca miei giochi!',
+        link2: 'Apri il mio GitHub',
+        link3: 'Sobre mi',
+        link4: 'Mi LinkedIn',
+        link5: 'CApicce',
+    }
+};
+
+function changeLanguage(lang) {
+    document.getElementById('title').innerHTML = translations[lang].title;
+    document.getElementById('text').textContent = translations[lang].description;
+    document.getElementById('Link1').textContent = translations[lang].link1;
+    document.getElementById('Link2').textContent = translations[lang].link2;
+    document.getElementById('Link3').textContent = translations[lang].link3;
+    document.getElementById('Link4').textContent = translations[lang].link4;
+    document.getElementById('Link5').textContent = translations[lang].link5;
+}
