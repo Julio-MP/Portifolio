41ab28145b25a0b00be0c88d3ad89ab685e64fb9
Julio Moreno Possebon <julio.possebon1@gmail.com>
2024-09-21 23:17:57 -0300

    Finalization

--- js/lang.js
+++ js/lang.js
@@ -1,7 +1,7 @@
 const translations = {
     'pt-br': {
         title: 'Olá! <br>Bem-vindo ao <strong class="Strong1">meu Portfólio!</strong>',
-        description: 'Aqui você pode encontrar alguns dos meus projetos concluídos',
+        description: 'Aqui você pode encontrar alguns dos meus projetos',
         link1: 'Jogue meus jogos!',
         link2: 'Abra meu GitHub',
         link3: 'Meu LinkedIn',
@@ -10,7 +10,7 @@ const translations = {
     },
     'en': {
         title: 'Hello!<br> Welcome to <strong class="Strong1">my Portfolio!</strong>',
-        description: 'Here you can find some of my finish projects and more:',
+        description: 'Here you can find some of my projects and more:',
         link1: 'Play my Games!',
         link2: 'Open my GitHub',
         link3: "My LinkedIn",
@@ -19,7 +19,7 @@ const translations = {
     },
     'it': {
         title: 'Ciao!<br> Benvenuto nel <strong class="Strong1">mio Portfolio!</strong>',
-        description: 'Qui puoi trovare alcuni dei miei progetti finiti',
+        description: 'Qui puoi trovare alcuni dei miei progetti',
         link1: 'Gioca miei giochi!',
         link2: 'Apri il mio GitHub',
         link3: 'Mi LinkedIn',
@@ -50,116 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
-// const translations = {
-//     'pt-br': {
-//         title: 'Olá! <br>Bem-vindo ao <strong class="Strong1">meu Portfólio!</strong>',
-//         description: 'Aqui você pode encontrar alguns dos meus projetos concluídos',
-//         link1: 'Jogue meus jogos!',
-//         link2: 'Abra meu GitHub',
-//         link3: 'Meu LinkedIn',
-//         link4: 'Sobre mim',
-//         link5: 'Entre em Contato',
-        
-//     },
-//     'en': {
-//         title: 'Hello!<br> Welcome to <strong class="Strong1">my Portfolio!</strong>',
-//         description: 'Here you can find some of my finish projects and more:',
-//         link1: 'Play my Games!',
-//         link2: 'Open my GitHub',
-//         link3: "My LinkedIn",
-//         link4: "About Me",
-//         link5: 'Contact me',
-        
-//     },
-//     'it': {
-//         title: 'Ciao!<br> Benvenuto nel <strong class="Strong1">mio Portfolio!</strong>',
-//         description: 'Qui puoi trovare alcuni dei miei progetti finiti',
-//         link1: 'Gioca miei giochi!',
-//         link2: 'Apri il mio GitHub',
-//         link3: 'Mi LinkedIn',
-//         link4: 'Sobre mi',
-//         link5: 'Contattami',
-        
-//     }
-// };
-
-
-
-// function changeLanguage(lang) {
-//     document.getElementById('title').innerHTML = translations[lang].title;
-//     document.getElementById('text').textContent = translations[lang].description;
-//     document.getElementById('Link1').textContent = translations[lang].link1;
-//     document.getElementById('Link2').textContent = translations[lang].link2;
-//     document.getElementById('Link3').textContent = translations[lang].link3;
-//     document.getElementById('Link4').textContent = translations[lang].link4;
-//     document.getElementById('Link5').textContent = translations[lang].link5;
-// }
-
-// -------------------------------------------------------------------------------/----------------------------------------------------------------//
-// function changeLanguage(lang) {
-//     if (window.location.pathname.includes('about.html')) {
-//         if (document.getElementById('title2')) {
-//             document.getElementById('title2').innerHTML = translations[lang].title2;
-//         }
-//         if (document.getElementById('back')) {
-//             document.getElementById('back').textContent = translations[lang].back;
-//         }
-//         if (document.getElementById('link')) {
-//             document.getElementById('link').textContent = translations[lang].link;
-//         }
-//         if (document.getElementById('about')) {
-//             document.getElementById('about').textContent = translations[lang].text;
-//         }
-//     } else {
-//         if (document.getElementById('title')) {
-//             document.getElementById('title').innerHTML = translations[lang].title;
-//         }
-        
-//         if (document.getElementById('text')) {
-//             document.getElementById('text').textContent = translations[lang].description;
-            
-//         }
-        
-//         if (document.getElementById('Link1')) {
-//             document.getElementById('Link1').textContent = translations[lang].link1;
-//         }
-        
-//         if (document.getElementById('Link2')) {
-//             document.getElementById('Link2').textContent = translations[lang].link2;
-//         }
-        
-//         if (document.getElementById('Link3')) {
-//             document.getElementById('Link3').textContent = translations[lang].link3;
-//         }
-
-//         if (document.getElementById('Link4')) {
-//             document.getElementById('Link4').textContent = translations[lang].link4;
-//         }
-
-//         if (document.getElementById('Link5')) {
-//             document.getElementById('Link5').textContent = translations[lang].link5;
-//         }
-//     }
-// }
-
-// function changeLanguage(lang) {
-//     document.getElementById('title').innerHTML = translations[lang].title;
-//     document.getElementById('text').textContent = translations[lang].description;
-//     document.getElementById('Link1').textContent = translations[lang].link1;
-//     document.getElementById('Link2').textContent = translations[lang].link2;
-//     document.getElementById('Link3').textContent = translations[lang].link3;
-//     document.getElementById('Link4').textContent = translations[lang].link4;
-//     document.getElementById('Link5').textContent = translations[lang].link5;
-    
-//     document.getElementById('title2').innerHTML = translations[lang].title2;
-//     document.getElementById('back').textContent = translations[lang].back;
-//     document.getElementById('link').textContent = translations[lang].link;
-//     document.getElementById('about').textContent = translations[lang].text;
-
-// }
 
 
 
--- js/langAbout.js
+++ js/langAbout.js
@@ -3,19 +3,22 @@ const translations = {
         title2: 'Sobre mim',
         back: 'Voltar',
         link: 'Entre em contato',
-        text: 'PT',
+        summary: 'Principais pontos fortes: <br> <strong class="Strong1">Curioso e dedicado!</strong>',
+        text: "Gosto de resolver e inventar coisas com criatividade. Escolhi a área desenvolvedor pois é possível criar nosso próprio mundo através da programação. Além disso busco a liberdade para resolver problemas diários e encontrar soluções criativas com a tecnologia, como já dizia Platão 'A necessidade é a mãe da invenção.'",
     },
     'en': {
         title2: 'About',
         back: 'Back',
         link: 'Contact me',
-        text: 'EN',
+        summary: 'Main strengths:<br> <strong class="Strong1">Curious and dedicated!</strong>',
+        text: "I enjoy solving and inventing things with creativity. I chose the developer field because it's possible to create our own world through programming. In addition, I seek the freedom to solve daily problems and find creative solutions with technology. As Plato once said, 'Necessity is the mother of invention.'",
     },
     'it': {
         title2: 'Sobre mi',
         back: 'Volte',
         link: 'Contattami',
-        text: 'ITAA',
+        summary: 'Principali punti di forza: <br> <strong class="Strong1">Curioso e dedicato!</strong>',
+        text: "Mi piace risolvere e inventare cose con creatività. Ho scelto l'area dello sviluppo perché è possibile creare il nostro mondo attraverso la programmazione. Inoltre, cerco la libertà di risolvere i problemi quotidiani e trovare soluzioni creative con la tecnologia. Come diceva Platone, 'La necessità è la madre dell'invenzione.'",
     }
 };
 
@@ -28,6 +31,7 @@ function changeLanguage(lang) {
     document.getElementById('back').textContent = translations[lang].back;
     document.getElementById('link').textContent = translations[lang].link;
     document.getElementById('about').textContent = translations[lang].text;
+    document.getElementById('summary').innerHTML = translations[lang].summary;
 }
 
 // Load the saved language on page load
